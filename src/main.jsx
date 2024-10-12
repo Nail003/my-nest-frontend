4bc40f9cb9ad3c8ce4a597fda20d55f0f821d146
Nail003 <danialzafarstudy003@gmail.com>
2024-10-12 19:33:47 +0500

    fix: update react router basepath

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { AppContextProvider, UserContextProvider } from "./contexts";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="my-nest-frontend">
             <AppContextProvider>
                 <UserContextProvider>
                     <Header />
--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,1 @@
-export const url = "http://localhost:3000";
+export const url = "https://minor-gui-nail003-a2839abb.koyeb.app";
